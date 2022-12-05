88d6cbc2651b7071a4debf2203de1ba91084fa4f
Chirath Pansilu <chirathpansilu@gmail.com>
2022-12-06 01:17:04 +0530

    Day 5 Challenge 2

---
+++ 5/2.py
@@ -0,0 +1,42 @@
+with open('stacks_input.txt', 'r') as reader:
+	file = reader.readlines()
+
+stacks = [[] for _ in range(9)]
+
+for l in file:
+	l = l.rstrip('\n').replace('[', ' ').replace(']', ' ')
+
+	for i in range(len(l)):
+		if l[i] != ' ':
+			stacks[i//4].append(l[i])
+
+ins = []
+with open('instructions.txt', 'r') as reader:
+	for l in reader.readlines():
+		t = []
+		for s in l.split():
+			if s.isdigit():
+				t.append(int(s))
+		ins.append(t)
+
+for i in ins:
+	f = i[1] - 1
+	t = i[2] - 1
+	c = i[0]
+
+	r = (stacks[f][:c])
+	r.extend(stacks[t])
+	stacks[t] = r
+	
+	del stacks[f][:c]
+
+s = ''
+
+for l in stacks:
+	if len(l) != 0:
+		s += l[0]
+
+print(s)
+
+with open('output.txt', 'w') as writer:
+	writer.write(s)
